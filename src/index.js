755c307df44c0614f5386ac93c15e09bd23d9e94
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-05-19 11:05:30 +0300

    Add data to API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './index.css';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QAONJ05W80sFwCly3Cb9/scores/'
+console.log(url)
+const nameInput = document.querySelector('.name');
+const scoreInput = document.querySelector('.score');
+const submitBtn = document.querySelector('.submit-btn');
+
+const sendData = async (userName, score) => {
+        await fetch((url), {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+            body: JSON.stringify({
+                userName,
+                score
+            })
+      
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+}
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const nameInput = document.querySelector('.name').value;
+    const scoreInput = document.querySelector('.score').value;
+    sendData(nameInput, scoreInput);
+})
